81571a5fd9f31ad49a0d5fb7cd14fdfcd0d7fde8
walterlv <xblero@gmail.com>
2018-10-10 16:18:47 +0800

    Add initial project files.

---
+++ SourceYard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceYard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
